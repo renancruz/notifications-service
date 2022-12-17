a2146e919e3a5552228d7b3ecab27855d08aa8bd
Renan Cruz <renan.frcr@gmail.com>
2022-12-17 11:26:56 -0300

    Infraestrutura e injeção de dependências

---
+++ src/application/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,30 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications';
+import { CountRecipientNotifications } from './count-recipient-notifications';
+
+describe('Count recipients notifications', () => {
+  it('should be able to count recipient notification', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const countRecipientNotifications = new CountRecipientNotifications(
+      notificationsRepository,
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-1' }),
+    );
+
+    await notificationsRepository.create(
+      makeNotification({ recipientId: 'recipient-2' }),
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipient-1',
+    });
+
+    expect(count).toEqual(2);
+  });
+});
---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notifications';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { UnreadNotification } from './unread-notification';
+
+describe('Unread notification', () => {
+  it('should be able to unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('should not be able to read a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    expect(async () => {
+      await unreadNotification.execute({
+        notificationId: 'fake-notificaton-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
